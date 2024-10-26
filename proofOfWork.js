a878f9adaf8b80c892e747f3de855507d1846519
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-26 23:31:09 +0530

    hash starting with a string

--- proofOfWork.js
+++ proofOfWork.js
@@ -1,10 +1,12 @@
 // Code to get a value that outputs sha256 hash with '00000' as the initials.
+// You will have to brute force until you find a value that starts with 00000
 const crypto = require('crypto');
 
 function findHashWithPrefix(prefix) {
   let input = 0;
   while (true) {
-    let inputStr = input.toString();
+    // What if I ask you that the input string should start with 'Saksham' ? How would the code change?
+    let inputStr = 'Saksham' + input.toString();
     let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
 
     if (hash.startsWith(prefix)) {
