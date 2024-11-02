0720d06e90029f5d2d05734eb5532972000cf3a3
Saksham Tewari <sakshamtewari3@gmail.com>
2024-11-02 22:19:27 +0530

    Enum, use of '_'

--- Rust/src/bin/enums.rs
+++ Rust/src/bin/enums.rs
@@ -1,49 +1,28 @@
 fn main() {
-    let my_direction = Direction::North;
+    let _my_direction = Direction::North;
     println!(
         "Area of Rectangle is {}",
         print_area(Shape::Rectangle(5.0, 5.0))
     );
-
-    let index = find_first_a(String::from("Saksham"));
-    match index {
-        Some(val) => println!("Index is {}", val),
-        None => println!("Not found"),
-    };
 }
 
 enum Direction {
     North,
-    East,
-    South,
-    West,
+    _East,
+    _South,
+    _West, // '_' is used to denote 'unused'
 }
 
 // Enums with values
 enum Shape {
     Rectangle(f64, f64),
-    Circle(f64),
+    _Circle(f64),
 }
 
 // f64 = floating 64 bits
 fn print_area(shape: Shape) -> f64 {
     match shape {
         Shape::Rectangle(a, b) => a * b,
-        Shape::Circle(r) => 3.14 * r * r,
+        Shape::_Circle(r) => 3.14 * r * r,
     }
-}
-
-// Options/Result Enum
-
-/*
-    Options enum : lets you return 'some' value or 'none' value
-*/
-
-fn find_first_a(s: String) -> Option<i32> {
-    for (index, character) in s.chars().enumerate() {
-        if character == 'a' {
-            return Some(index as i32);
-        }
-    }
-    return None;
 }
