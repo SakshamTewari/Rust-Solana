70c70d7219ec911226624416333ce7fa96dbfe93
Saksham Tewari <sakshamtewari3@gmail.com>
2025-08-16 19:17:34 +0530

    update: vector filter, initialize method

--- Rust/src/bin/vector.rs
+++ Rust/src/bin/vector.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let mut vec = Vec::new();
-    vec.push(1);
-    vec.push(2);
-    println!("{:?}", vec); // {:?} is used to print a struct (as Vector is a struct)
+    let mut vec1 = Vec::new();
+    let mut vec2 = vec![1,2,3,4];  // another way to intialize
+    vec1.push(1);
+    vec1.push(2);
+    println!("{:?}", vec1); // {:?} is used to print a struct (as Vector is a struct)
+    println!("{:?}", even_filter(vec1))
+}
+
+fn even_filter(vec: Vec<i32>) -> Vec<i32> {
+    let mut new_vec = Vec::new();
+    for val in vec {
+        if val %2 == 0 {
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec;
 }
